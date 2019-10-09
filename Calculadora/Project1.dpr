292f675eb0ecf1d267fa0876196858943006dfa5
ma-spec <56327289+ma-spec@users.noreply.github.com>
2019-10-08 22:10:28 -0300

    Add files via upload

---
+++ Calculadora/Project1.dpr
@@ -0,0 +1,14 @@
+program Project1;
+
+uses
+  Vcl.Forms,
+  Unit1 in 'Unit1.pas' {Calculadora};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.MainFormOnTaskbar := True;
+  Application.CreateForm(TCalculadora, Calculadora);
+  Application.Run;
+end.
